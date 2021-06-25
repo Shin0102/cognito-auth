61aa72d93d8ffe85dee32466fb8943a4b21055cb
Alli <shinalli0102@gmail.com>
2021-06-25 15:53:42 +0900

    Revert "Remove error handler for debug"
    This reverts commit 07c2d041210cd0383233ce317979ffcf3f5920b5.

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -42,4 +42,30 @@ export default ({ app }: { app: express.Application }) => {
 
   // Load API routes
   app.use(config.api.prefix, routes());
+
+  /// catch 404 and forward to error handler
+  app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err['status'] = 404;
+    next(err);
+  });
+
+  /// error handlers
+  app.use((err, req, res, next) => {
+    /**
+     * Handle 401 thrown by express-jwt library
+     */
+    if (err.name === 'UnauthorizedError') {
+      return res.status(err.status).send({ message: err.message }).end();
+    }
+    return next(err);
+  });
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+      errors: {
+        message: err.message,
+      },
+    });
+  });
 };
