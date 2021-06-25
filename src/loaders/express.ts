a640f68b3a31a5d4e84af54c95b06b17377c6b95
Alli <shinalli0102@gmail.com>
2021-06-25 15:24:34 +0900

    Add cors credentails true

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import routes from '../api';
 import config from '../config';
+import Logger from './logger';
 
 export default ({ app }: { app: express.Application }) => {
   /**
@@ -24,7 +25,12 @@ export default ({ app }: { app: express.Application }) => {
   // The magic package that prevents frontend developers going nuts
   // Alternate description:
   // Enable Cross Origin Resource Sharing to all origins by default
-  app.use(cors());
+  app.use(
+    cors({
+      origin: true,
+      credentials: true,
+    })
+  );
 
   // Some sauce that always add since 2014
   // "Lets you use HTTP verbs such as PUT or DELETE in places where the client doesn't support it."
